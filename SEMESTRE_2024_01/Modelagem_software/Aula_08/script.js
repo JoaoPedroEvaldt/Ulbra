a5404fbdb6b53cb54a29d65d75504b1edda62b89
João Pedro Evaldt <joao.oliveiraevaldt@gmail.com>
2024-06-05 19:30:24 -0300

    Atualizando

---
+++ SEMESTRE_2024_01/Modelagem_software/Aula_08/script.js
@@ -0,0 +1,41 @@
+class Cliente {
+
+    constructor(nome, endereco, teleofone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.teleofone = teleofone;
+    }
+
+    salvar() {
+
+        let cliente = {
+            nome: this.nome,
+            endereco: this.endereco,
+            telefone: this.teleofone
+        }
+
+
+        return
+    }
+
+    alterar() {
+
+        let cliente = {
+            nome: this.nome,
+            endereco: this.endereco,
+            telefone: this.teleofone
+        }
+
+        excluir()
+        {
+            try {
+                return "Deu tudo certo"
+            } catch (error) {
+
+            }
+
+
+        }
+
+    }
+}
